9c43b416d10b8f4ec743bc56c02a91ccdb5a61e9
Blancaaaaaaa <blanatimar@gmail.com>
2024-12-09 12:47:38 +0100

    SaaS siguiendo el orientado a objetos

---
+++ borrar.php
@@ -0,0 +1,357 @@
+<!-- @Author: Blanca Atienzar Martinez (HTML, CSS y funcionalidad de SaaS) -->
+
+<?php session_start() ;
+include "conexion.php";
+$conn = Conexion::getConnection();   
+if (isset($_SESSION['success_msg'])) {
+    echo "<div class='alert alert-success' role='alert'>{$_SESSION['success_msg']}</div>";
+    unset($_SESSION['success_msg']);
+}
+if (isset($_SESSION['error_msg'])) {
+    echo "<div class='alert alert-danger' role='alert'>{$_SESSION['error_msg']}</div>";
+    unset($_SESSION['error_msg']);
+}           
+?>
+
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>MPHB</title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins|Raleway:400,600|Righteous&display=swap"
+        rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container">
+                    <a class="navbar-brand" href="loginform.php">
+                        <span>
+                            MPHB
+                        </span>
+                    </a>
+
+                    <div class="navbar-collapse" id="">
+
+                        <div class="custom_menu-btn">
+                            <button onclick="openNav()">
+                                <span class="s-1"> </span>
+                                <span class="s-2"> </span>
+                                <span class="s-3"> </span>
+                            </button>
+                        </div>
+                        <div id="myNav" class="overlay">
+                            <div class="overlay-content">
+                                <a href="loginform.php">Home</a>
+                            </div>
+                            <div class="overlay-content">
+                                <a href="servicesform.php">Services</a>
+                            </div>
+                            <div class="overlay-content">
+                                <a href="servicesSaaSPersonalform.php">SaaS</a>
+                            </div>
+                            <div class="overlay-content">
+                                <a href="servicesPaaSPersonalform.php">PaaS</a>
+                            </div>  
+                            <div class="overlay-content">
+                                <a href="gestOrgform.php">Gestionar Organitzacións</a>
+                            </div>                       
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- end header section -->
+    </div>
+
+    <!-- about section -->
+
+    <section class="about_section layout_paddingAbout">
+        <div class="container">
+            <h2 class="text-uppercase">
+                Servicios SaaS - Eliminar
+            </h2>
+            <form>
+                <div class="container">
+                <button type="submit" class="btn btn-primary" formaction="servicesSaaSViewform.php">Inicio</button>
+                    <button type="submit" class="btn btn-primary" formaction="servicesSaaSPersonalform.php">Contratos SaaS</button>
+                    <button type="submit" class="btn btn-primary" formaction="servicesSaaSCreateform.php">Crear</button>
+                    <button type="submit" class="btn btn-primary" formaction="servicesSaaSDeleteform.php">Eliminar</button>
+                    <button type="submit" class="btn btn-primary" formaction="servicesSaaSTestform.php">Test</button>
+                </div>
+            </form>
+        </div>
+    
+        
+        <h3>Componentes de los productos SaaS</h3> 
+        <form id="formulari" action=" " method="POST">
+            <div class="container d-flex justify-content-center ">
+                <div class="form-row">
+                    <div class="col-auto">
+                        <!-- Tabla para tipusMCMS -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Modulo CMS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM MODUL_CMS";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['tipus'] ;
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <!-- Tabla para tipusCDN -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Tipo de CDN</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM CDN";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['preu']. '|' . $row['tipus'];
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                        <td>{$row['preu']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-auto">
+                        <!-- Tabla para tipusSSL -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Tipo de SSL</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM C_SSL";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['preu']. '|' . $row['tipus'];
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                        <td>{$row['preu']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <!-- Tabla para tipusSGBD -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Tipo de SGBD</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM SIST_GESTIO_BD";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['tipus'];
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-auto">    
+                        <!-- Tabla para tipusRam -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Tipo de Ram</th>
+                                    <th>GB de Ram</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM RAM";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['preu']. '|' . $row['tipus'] . '|' . $row['GB'];
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                        <td>{$row['GB']}</td>
+                                        <td>{$row['preu']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <!-- Tabla para tipusDD -->
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Selecciona</th>
+                                    <th>Tipo de DD</th>
+                                    <th>GB de DD</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $sql = "SELECT * FROM DISC_DUR";
+                                $result = mysqli_query($conn, $sql);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $value = $row['preu']. '|' . $row['tipus'] . '|' . $row['GB'];
+                                    echo "<tr>
+                                        <td>
+                                            <input type='radio' id='selectedRow' name='selectedRow' value='{$value}'>
+                                        </td>
+                                        <td>{$row['tipus']}</td>
+                                        <td>{$row['GB']}</td>
+                                        <td>{$row['preu']}</td>
+                                    </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                </div>
+            </div>
+        <input type="text" class="form form-primary mb-2" id="testName" name="testName" placeholder="    9.99" required>
+        <button type="button" class="btn btn-primary mt-3" id="botEditarComp" name="action" value="editarComp">Editar Precio Componente</button>
+        <button type="button" class="btn btn-primary mt-3" id="botBorrarComp" name="action" value="deleteComp">Eliminar Componente</button>
+        </form>
+            <script>
+                const form = document.getElementById('formulari');
+                const botEditar = document.getElementById('botEditarComp');
+                const botBorrar = document.getElementById('botBorrarComp');
+
+                // Función para verificar si se seleccionó un radio
+                function isRadioSelected() {
+                    const radios = document.querySelectorAll('input[name="selectedRow"]');
+                    for (let i = 0; i < radios.length; i++) {
+                        if (radios[i].checked) {
+                            return true;  // Si al menos uno está seleccionado
+                        }
+                    }
+                    return false;  // Ningún radio está seleccionado
+                }
+
+                // Cambiar la acción del formulario dependiendo del botón clicado
+                botEditar.addEventListener('click', function() {
+                    const selectedValue = isRadioSelected();
+                    if (selectedValue) {
+                        if (selectedValue.includes('|')) {
+                            alert('TIENE PRECIO');
+                            form.action = 'servicesSaaSDeletehpaBD.php';
+                            form.submit();
+                            
+                        } else {
+                            alert('Por favor, selecciona un componente con precio para editar.');
+                        }
+                    } else {
+                        alert('Por favor, selecciona un componente para editar.');
+                    }
+                });
+
+                botBorrar.addEventListener('click', function() {
+                    const selectedValue = isRadioSelected();
+                    if (selectedValue) {
+                        form.action = 'servicesSaaSDeletehpaBD.php';
+                        form.submit();
+                    } else {
+                        alert('Por favor, selecciona un componente para borrar.');
+                    }
+                });
+            </script>
+    </section>
+
+    <!-- end about section -->
+
+
+    <!-- footer section -->
+    <section class="container-fluid footer_section">
+        <p>
+            &copy; 2024 (UIB - EPS). Design by MPHB
+        </p>
+    </section>
+    <!-- footer section -->
+
+    <!--script type="text/javascript" src="js/jquery-3.4.1.min.js"></script-->
+    <!--script type="text/javascript" src="js/bootstrap.js"></script-->
+
+    <script>
+        function openNav() {
+            document.getElementById("myNav").classList.toggle("menu_width");
+            document
+                .querySelector(".custom_menu-btn")
+                .classList.toggle("menu_btn-style");
+        }
+    </script>
+</body>
+
+</html>
