950ce23f17668198e0c8b01507e88f074f019209
Blanca <115868584+blancaatienzar@users.noreply.github.com>
2024-12-11 18:54:09 +0100

    Falta POO Componentes eliminar y editar

--- src/vista/componentesVista.php
+++ src/vista/componentesVista.php
@@ -1,7 +1,7 @@
 <!-- @Author: Blanca Atienzar Martinez (HTML, CSS y funcionalidad de SaaS) -->
 <?php
 session_start();
-require_once  './../componentes.php';
+require_once './../componentes.php';
 
 $accion = $_POST['accio'];
 
@@ -13,11 +13,19 @@ switch ($accion) {
     case 'eliminarS':
         eliminarSaaS();
         break;
+    case 'editarS':
+        editarSaaS();
+        break;
 }
 
-
 function crearSaaS() {
+    $component = $_POST['component'];
+    $tipo = isset($_POST['tipo']) ? $_POST['tipo'] : null;
+    $tipotipo = isset($_POST['tipotipo']) ? $_POST['tipotipo'] : null;
+    $gb = isset($_POST['gb']) ? $_POST['gb'] : null;
+    $precio = isset($_POST['precio']) ? $_POST['precio'] : null;
     
+    $res = Componentes::crearComponentesSaas($component, $tipo, $tipotipo, $gb, $precio);
 
     // Redirecciona a la pagina 
     header("Location: ./../../servicesSaaSComponentesform.php");
@@ -25,13 +33,40 @@ function crearSaaS() {
 }
 
 function eliminarSaaS() {
-    $testName = $_POST['noms']; 
+    $selectedRow = $_POST['selectedRow'];
+    list($componente, $tipo, $precio, $gb) = explode('|', $selectedRow);
+    echo "Debug: eliminarSaaS - Componente: $componente, Tipo: $tipo, Precio: $precio, GB: $gb<br>";
 
-    $res = Test::eliminarTestSaas($testName);
 
-    // Redirecciona a la pagina 
-    header("Location: ./../../servicesSaaSComponentesform.php");
-    exit();
+    $res = Componentes::eliminarComponentesSaas($componente, $tipo, $gb);
+
+    if ($res) {
+        // Redirecciona a la pagina 
+        header("Location: ./../../servicesSaaSComponentesform.php");
+        exit();
+    } else {
+        echo "Error al eliminar el componente.";
+    }
 }
 
+function editarSaaS() {
+    $selectedRow = explode('|', $_POST['selectedRow']);
+    $componente = $selectedRow[0];
+    $tipo = $selectedRow[1];
+    $precio = $selectedRow[2];
+    $gb = $selectedRow[3];
+    $precioCambiar = $_POST['precio'];
+    echo "Debug: eliminarSaaS - Componente: $componente, Tipo: $tipo, Precio: $precio, GB: $gb<br>";
+
+
+    $res = Componentes::editarComponentesSaas($componente, $tipo, $gb, $precioCambiar);
+
+    if ($res) {
+        // Redirecciona a la pagina 
+        header("Location: ./../../servicesSaaSComponentesform.php");
+        exit();
+    } else {
+        echo "Error al editar el componente.";
+    }
+}
 ?>
